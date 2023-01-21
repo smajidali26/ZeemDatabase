431c4330d428e8d73a382814b379f7f2ecc694fb
Majid Ali <majid.ali@synergy-it.com>
2023-01-22 01:17:02 +0500

    Added First Time Script

---
+++ Zeem/Zeem/Script/FirstTimeDeploymentScript.sql
@@ -0,0 +1,32 @@
+﻿GO
+IF (NOT EXISTS(SELECT * FROM ZeemRole))
+BEGIN
+	INSERT INTO [dbo].[ZeemRole]
+	(
+		[RoleName],
+		[SelfRegistrationRole],
+		[CreatedBy],
+		[CreatedDate]		
+	)
+	VALUES 
+		(
+			'Super Administrator',
+			0,
+			1,
+			GETDATE()
+		),
+		(
+			'Administrator',
+			0,
+			1,
+			GETDATE()
+		),
+		(
+			'Client',
+			1,
+			1,
+			GETDATE()
+		)
+END
+
+GO
